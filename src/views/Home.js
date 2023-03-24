f257ed38962aec665588afe1e51a189a717b64e2
Josutj <91794492+Josutj@users.noreply.github.com>
2023-03-24 01:05:25 -0600

    dashboard

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
 import NavBar from "../components/globals/NavBar";
-import TableAsociacion from "../components/Table/TableUsers";
+// import TableAsociacion from "../components/Table/TableUsers";
 import "./Home.scss";
+import dashboard from "../imgs/dashboard.jpeg"
 
 const Home = () => {
   return (
     <div className="container-info">
        <NavBar titule='Inicio' />
       <div id="" className="">
-        <h1>Home</h1>
-        {/* <TableAsociacion /> */}
+        <h1>DASHBOARD DEL PRODUCTO</h1>
+        <img className="dhb" src={dashboard} alt="no image"></img>
       </div>
     </div>
   );
